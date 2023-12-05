e28488868ab9b1621183f8b63efbf3dd75a821da
Audun Nes <aunes@dfds.com>
2023-12-05 18:55:34 +0100

    Use an environment variable for the instance name in cleanup script for cloudwatch

--- tests/qa/cloudwatch_log_groups.sh
+++ tests/qa/cloudwatch_log_groups.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+RDS_INSTANCE_NAME=qa; export RDS_INSTANCE_NAME
+
 aws logs describe-log-groups --region eu-central-1 | jq -r '.logGroups[].logGroupName' | while read file
 do
-    if [[ $file == "/aws/rds/instance/qa/postgresql" ]] || [[ $file == "/aws/rds/instance/qa/upgrade" ]] || [[ $file == "/aws/rds/proxy/qa" ]]
+    if [[ $file == "/aws/rds/instance/${RDS_INSTANCE_NAME}/postgresql" ]] || [[ $file == "/aws/rds/instance/${RDS_INSTANCE_NAME}/upgrade" ]] || [[ $file == "/aws/rds/proxy/${RDS_INSTANCE_NAME}" ]]
     then
         aws logs delete-log-group --log-group-name $file
     fi
