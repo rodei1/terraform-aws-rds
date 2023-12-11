d09880119af690e3c9904ad20fe6b89b4d582764
samidbb <40062553+samidbb@users.noreply.github.com>
2023-12-11 11:29:34 +0100

    print deletion info in script (#35)

--- tests/qa/cloudwatch_log_groups.sh
+++ tests/qa/cloudwatch_log_groups.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 RDS_INSTANCE_NAME=qa; export RDS_INSTANCE_NAME
 
 aws logs describe-log-groups --region eu-central-1 | jq -r '.logGroups[].logGroupName' | while read file
 do
     if [[ $file == "/aws/rds/instance/${RDS_INSTANCE_NAME}/postgresql" ]] || [[ $file == "/aws/rds/instance/${RDS_INSTANCE_NAME}/upgrade" ]] || [[ $file == "/aws/rds/proxy/${RDS_INSTANCE_NAME}" ]]
     then
+        echo "Deleting log group: $file"
         aws logs delete-log-group --log-group-name $file
     fi
 done
