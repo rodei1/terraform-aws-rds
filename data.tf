044fe566bdf5bf72114ddfa880d7549355dda45b
Aleksandra Korczak <alekorc@dfds.com>
2023-12-12 14:52:58 +0100

    Update aws_rds_engine_version data source

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_rds_engine_version" "default" {
   engine       = local.engine
   version      = var.engine_version
-  default_only = true
+  default_only = var.engine_version != null ? false : true
 }
